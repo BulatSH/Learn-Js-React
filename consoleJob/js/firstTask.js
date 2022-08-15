2c3c7ae5e8c335e4d9d42e89e8468c94fa21682c
Bulat <hufdgdgdgfd123@gmail.com>
2022-08-16 00:57:28 +0300

    cycles, contitions, operators, functions, methods and properties

--- consoleJob/js/firstTask.js
+++ consoleJob/js/firstTask.js
@@ -1,4 +1,9 @@
+'use strict';
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let oneOfTheLastFirstFilm = '',
+    oneOfTheLastFirstFilmRate = '';
+let questions = 2;
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,11 +13,56 @@ const personalMovieDB = {
     private: false
 };
 
-const oneOfTheLastFirstFilm = prompt('Один из последних просмотренных фильмов?', ''),
+// point: for (let i = 0; i < questions; i++) {
+//     oneOfTheLastFirstFilm = prompt('Один из последних просмотренных фильмов?', '');
+//     oneOfTheLastFirstFilmRate = prompt('На сколько оцените его?', '');
+
+//     if (oneOfTheLastFirstFilm != null && oneOfTheLastFirstFilmRate != null && 
+//         oneOfTheLastFirstFilm != '' && oneOfTheLastFirstFilmRate != '' && oneOfTheLastFirstFilm.length < 50) {
+//         personalMovieDB.movies[oneOfTheLastFirstFilm] = oneOfTheLastFirstFilmRate;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
+
+// while (questions !== 0) {
+//     oneOfTheLastFirstFilm = prompt('Один из последних просмотренных фильмов?', '');
+//     oneOfTheLastFirstFilmRate = prompt('На сколько оцените его?', '');
+
+//     if (oneOfTheLastFirstFilm != null && oneOfTheLastFirstFilmRate != null && 
+//         oneOfTheLastFirstFilm != '' && oneOfTheLastFirstFilmRate != '' && oneOfTheLastFirstFilm.length < 50) {
+//         personalMovieDB.movies[oneOfTheLastFirstFilm] = oneOfTheLastFirstFilmRate;
+//         console.log('done');
+//         questions--;
+//     } else {
+//         console.log('error');
+//     }
+// }
+
+do {
+    oneOfTheLastFirstFilm = prompt('Один из последних просмотренных фильмов?', '');
     oneOfTheLastFirstFilmRate = prompt('На сколько оцените его?', '');
 
-const oneOfTheLastSecondFilm = prompt('Один из последних просмотренных фильмов?', ''),
-    oneOfTheLastSecondFilmRate = prompt('На сколько оцените его?', '');
+    if (oneOfTheLastFirstFilm != null && oneOfTheLastFirstFilmRate != null && 
+        oneOfTheLastFirstFilm != '' && oneOfTheLastFirstFilmRate != '' && oneOfTheLastFirstFilm.length < 50) {
+        personalMovieDB.movies[oneOfTheLastFirstFilm] = oneOfTheLastFirstFilmRate;
+        console.log('done');
+        questions--;
+    } else {
+        console.log('error');
+    }
+} while (questions !== 0);
 
-personalMovieDB.movies[oneOfTheLastFirstFilm] = oneOfTheLastFirstFilmRate;
-personalMovieDB.movies[oneOfTheLastSecondFilm] = oneOfTheLastSecondFilmRate;
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
+console.log(personalMovieDB);
