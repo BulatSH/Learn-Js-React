dd76beed609fb46985d528cbca60271e87f0d038
Bulat <hufdgdgdgfd123@gmail.com>
2022-08-22 17:52:12 +0300

    Application improvement. Adding functions.

--- consoleJob/js/firstTask.js
+++ consoleJob/js/firstTask.js
@@ -1,8 +1,17 @@
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-let oneOfTheLastFirstFilm = '',
-    oneOfTheLastFirstFilmRate = '';
+let numberOfFilms;
+
+function start () {
+    numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
+
 let questions = 2;
 
 const personalMovieDB = {
@@ -13,20 +22,55 @@ const personalMovieDB = {
     private: false
 };
 
-// point: for (let i = 0; i < questions; i++) {
-//     oneOfTheLastFirstFilm = prompt('Один из последних просмотренных фильмов?', '');
-//     oneOfTheLastFirstFilmRate = prompt('На сколько оцените его?', '');
+function rememberMyFilms () {
+    for (let i = 0; i < questions; i++) {
+        const oneOfTheLastFirstFilm = prompt('Один из последних просмотренных фильмов?', ''),
+        oneOfTheLastFirstFilmRate = prompt('На сколько оцените его?', '');
+    
+        if (oneOfTheLastFirstFilm != null && oneOfTheLastFirstFilmRate != null && 
+            oneOfTheLastFirstFilm != '' && oneOfTheLastFirstFilmRate != '' && oneOfTheLastFirstFilm.length < 50) {
+            personalMovieDB.movies[oneOfTheLastFirstFilm] = oneOfTheLastFirstFilmRate;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
 
-//     if (oneOfTheLastFirstFilm != null && oneOfTheLastFirstFilmRate != null && 
-//         oneOfTheLastFirstFilm != '' && oneOfTheLastFirstFilmRate != '' && oneOfTheLastFirstFilm.length < 50) {
-//         personalMovieDB.movies[oneOfTheLastFirstFilm] = oneOfTheLastFirstFilmRate;
-//         console.log('done');
-//     } else {
-//         console.log('error');
-//         i--;
-//     }
-// }
+rememberMyFilms();
 
+function detectPersonalLevel () {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
+
+detectPersonalLevel();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.private);
+
+function writeYourGenres () {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}` , '');
+    }
+}
+
+writeYourGenres();
+
+// Разные варианты циклов
 // while (questions !== 0) {
 //     oneOfTheLastFirstFilm = prompt('Один из последних просмотренных фильмов?', '');
 //     oneOfTheLastFirstFilmRate = prompt('На сколько оцените его?', '');
@@ -41,28 +85,16 @@ const personalMovieDB = {
 //     }
 // }
 
-do {
-    oneOfTheLastFirstFilm = prompt('Один из последних просмотренных фильмов?', '');
-    oneOfTheLastFirstFilmRate = prompt('На сколько оцените его?', '');
+// do {
+//     oneOfTheLastFirstFilm = prompt('Один из последних просмотренных фильмов?', '');
+//     oneOfTheLastFirstFilmRate = prompt('На сколько оцените его?', '');
 
-    if (oneOfTheLastFirstFilm != null && oneOfTheLastFirstFilmRate != null && 
-        oneOfTheLastFirstFilm != '' && oneOfTheLastFirstFilmRate != '' && oneOfTheLastFirstFilm.length < 50) {
-        personalMovieDB.movies[oneOfTheLastFirstFilm] = oneOfTheLastFirstFilmRate;
-        console.log('done');
-        questions--;
-    } else {
-        console.log('error');
-    }
-} while (questions !== 0);
-
-if (personalMovieDB.count < 10) {
-    console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) {
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка');
-}
-
-console.log(personalMovieDB);
+//     if (oneOfTheLastFirstFilm != null && oneOfTheLastFirstFilmRate != null && 
+//         oneOfTheLastFirstFilm != '' && oneOfTheLastFirstFilmRate != '' && oneOfTheLastFirstFilm.length < 50) {
+//         personalMovieDB.movies[oneOfTheLastFirstFilm] = oneOfTheLastFirstFilmRate;
+//         console.log('done');
+//         questions--;
+//     } else {
+//         console.log('error');
+//     }
+// } while (questions !== 0);
