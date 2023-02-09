6558525a48eefc17ff90468f403fa28275d580df
Bulat Sharafutdinov <hufdgdgdgfd@mail.ru>
2023-02-09 23:18:16 +0300

    fix: practice

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = () => {
+	return <h2>Hello World!</h2>
 }
 
+// const Field = () => {
+// 	const holder = 'Enter here';
+// 	const styleField = {
+// 		width: '300px',
+// 	};
+//
+// 	return <input
+// 		placeholder={holder}
+// 		type="text"
+// 		style={styleField} />
+// }
+
+// Наследуемся от компонента
+class Field extends Component {
+	render() {
+		const holder = 'Enter here';
+		const styleField = {
+			width: '300px',
+		};
+
+		return <input
+			placeholder={holder}
+			type="text"
+			style={styleField} />
+	}
+}
+
+function Btn() {
+	const text = 'Log in';
+	// const res = () => {
+	// 	return 'Log in';
+	// }
+
+	// const p = <p>Log in</p>
+
+	const logged = true;
+
+	return <button>{logged ? 'Enter' : text}</button>
+}
+
+function App() {
+	return (
+		<div className="App">
+			{/*<header className="App-header">*/}
+			{/*  <img src={logo} className="App-logo" alt="logo" />*/}
+			{/*  <p>*/}
+			{/*    Edit <code>src/App.js</code> and save to reload.*/}
+			{/*  </p>*/}
+			{/*</header>*/}
+			<Header />
+			<Field />
+			<Btn />
+		</div>
+	);
+}
+
+export {Header};
 export default App;
--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import {Header} from "./App";
 
 // JS. Тип, классы и строка
 // const elem = React.createElement('h2', {className: 'color'}, 'Hello World!');
@@ -12,19 +13,30 @@ import App from './App';
 // Многострочный элемент
 // Всегда один родитель
 
-const text = 'Hello World!';
+// const text = 'Hello World!';
 
 // В контрукцию {} мы можем поместить любые переменные, кроме объектов
-const elem = (
-    <div>
-        <h2 className="test">Текст: {text} {2 + 2}</h2>
-        <input id="input" type="text" />
-        <label htmlFor="input"></label>
-        <button tabIndex={0}>Click</button>
-    </div>
-);
+// const elem = (
+//     <div>
+//         <h2 className="test">Текст: {text} {2 + 2}</h2>
+//         <input id="input" type="text" />
+//         <label htmlFor="input"></label>
+//         <button tabIndex={0}>Click</button>
+//     </div>
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// StrictMode нужен для выявления проблем в коде, он существует внутри Реакта
+// Помогает обнаруживать устаревшие и не безопасные конструкции
+// Работает только в разработке
 root.render(
-  elem
+	<StrictMode>
+		<App />
+	</StrictMode>
+	// elem
+    // Компоненты всегда должны быть написаны с большой буквы
 );
+// Компоненты это участки/блоки когда, которые могут быть переиспользованы
+// Компоненты это функции, которые внутри себя возвращают JSX элементы, и содержат внутри себя какое-то поведение
+// Компоненты пишутся в разных файлах и экспортируются и импортируются в другие файлы
